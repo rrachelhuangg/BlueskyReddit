9f48f44333b2acd8c7fe861427e0c8984cb1ee4e
Rachel Huang <rachelhuang@Rachels-Laptop.local>
2025-03-14 22:16:08 -0400

    Implemented fetching skeets from inputted accounts that were added from add component button. Also render account name properly. Should also implement error handling at some point.

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import requests
 
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, session
+from flask_cors import CORS
 from methods import get_refresh_token, get_access_token
 
 app = Flask(__name__)
+app.secret_key="SECRETKEY"
+CORS(app)
 
 @app.route("/")
 def home():
@@ -11,11 +14,13 @@ def home():
 
 @app.route("/", methods=["POST"])
 def access_login_info():
+    session['added_accounts'] = []
     if 'username' in request.form and 'password' in request.form:
         username = request.form['username']
         password = request.form['password']
         refresh_token = get_refresh_token(username, password)
         access_token = get_access_token(refresh_token)
+        session['access_token'] = access_token
         handle = "demo-bluesky-acc.bsky.social"
         url = f'https://bsky.social/xrpc/app.bsky.feed.getAuthorFeed?actor={handle}&limit={100}'
         headers = {
@@ -23,10 +28,10 @@ def access_login_info():
             'Content-Type':'application/json'
         }
         response = requests.get(url, headers=headers, timeout=15)
-
         posts = []
         for i in range(len(response.json()['feed'])):
             post = {}
+            post['name'] = response.json()['feed'][i]['post']['author']['displayName']
             post['handle'] = response.json()['feed'][i]['post']['author']['handle']
             post['text_content'] = response.json()['feed'][i]['post']['record']['text']
             post['reply_count'] = response.json()['feed'][i]['post']['replyCount']
@@ -34,6 +39,29 @@ def access_login_info():
             post['like_count'] = response.json()['feed'][i]['post']['likeCount']
             posts += [post]
         return render_template("postlogin.html", posts=posts)
+    elif 'account' in request.form:
+        access_token = session['access_token']
+        handle = request.form['account']
+        session['added_accounts'] += [handle] #should implement some sort of a cache to speed things up!
+        for handle in session['added_accounts']:
+            url = f'https://bsky.social/xrpc/app.bsky.feed.getAuthorFeed?actor={handle}&limit={100}'
+            headers = {
+                'Authorization':f"Bearer {access_token}",
+                'Content-Type':'application/json'
+            }
+            response = requests.get(url, headers=headers, timeout=15)
+            posts = []
+            for i in range(len(response.json()['feed'])):
+                post = {}
+                if response.json()['feed'][i]['post']['author']['handle'] == handle: #filtering out random posts??
+                    post['name'] = response.json()['feed'][i]['post']['author']['displayName']
+                    post['handle'] = response.json()['feed'][i]['post']['author']['handle']
+                    post['text_content'] = response.json()['feed'][i]['post']['record']['text']
+                    post['reply_count'] = response.json()['feed'][i]['post']['replyCount']
+                    post['repost_count'] = response.json()['feed'][i]['post']['repostCount']
+                    post['like_count'] = response.json()['feed'][i]['post']['likeCount']
+                    posts += [post]
+            return render_template("postlogin.html", posts=posts)
     else:
         return render_template("postlogin.html")
 
--- static/js/postlogin.js
+++ static/js/postlogin.js
@@ -4,6 +4,18 @@ const closeComponentModal = document.getElementById('close-modal-button');
 const closeComponentModal2 = document.getElementById('close-add-component-button');
 const addButton = document.getElementById('apply-changes-button');
 
+const addAccount = document.getElementById('add-account-button');
+addAccount.addEventListener('click', function(){
+    const addedAccount = addAccount.value;
+    fetch('/',{
+        method:'POST',
+        headers:{
+            'Content-Type': 'application/json'
+        },
+        body:JSON.stringify({account:addedAccount})
+    }).then(response=>response.json()).then(data=>{console.log("SUCCESS: ", addedAccount);}).catch(error=>{console.error("ERROR: ", error);})
+})
+
 function openAddComponentModal(){
     addComponentModal.style.display='flex';
 }
